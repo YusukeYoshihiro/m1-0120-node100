66cf8cc1d821fc7ea2480c263377f4510ef962ac
Francois Polo <andasan@gmail.com>
2020-09-01 10:57:21 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('Hello from NodeJS');
+// console.log('Bye');
+// console.log(__dirname, __filename);
+
+const Person = require('./person');
+
+const person1 = new Person('Mike',20);
+
+person1.greeting();
+
